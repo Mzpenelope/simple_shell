1b906b877f515a0a56d05318ff8e107f625488ff
rosyrose1 <128856014+rosyrose1@users.noreply.github.com>
2023-06-20 12:29:12 +0000

    Update phiros8.c

--- phiros8.c
+++ phiros8.c
@@ -194,44 +194,4 @@ b = waitpid(a, &s, WUNTRACED);
 dsh->status = s / 256;
 return (1);
 }
-/**
- * crosed - changes cd
- ** @dsh: data
- ** Return: 1 on success
- ****************************/
-int crosed(phiros_shell *dsh)
-{
-char *d;
-int a, b, c;
 
-d = dsh->ag[1];
-
-if (d != NULL)
-{
-a = _roscmp("$HOME", d);
-b = _roscmp("~", d);
-c = _roscmp("--", d);
-}
-
-if (d == NULL || !a || !b || !c)
-{
-rome(dsh);
-return (1);
-}
-
-if (_roscmp("-", d) == 0)
-{
-phivious(dsh);
-return (1);
-}
-
-if (_roscmp(".", d) == 0 || _roscmp("..", d) == 0)
-{
-phidot(dsh);
-return (1);
-}
-
-roto(dsh);
-
-return (1);
-}
