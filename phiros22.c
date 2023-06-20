70eb1cff6c4bf6eaa347c8b5f4f3596dcc3dadaf
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-20 17:18:04 +0100

    Update phiros22.c

--- phiros22.c
+++ phiros22.c
@@ -34,13 +34,13 @@ int pnv(phiros_shell *dsh)
 {
 int a, b;
 
-for (a = 0; dsh->roviron[a]; a++)
+for (a = 0; dsh->envi[a]; a++)
 {
 
-for (b = 0; dsh->roviron[a][b]; b++)
+for (b = 0; dsh->envi[a][b]; b++)
 ;
 
-write(STDOUT_FILENO, dsh->roviron[a], b);
+write(STDOUT_FILENO, dsh->envi[a], b);
 write(STDOUT_FILENO, "\n", 1);
 }
 dsh->status = 0;
