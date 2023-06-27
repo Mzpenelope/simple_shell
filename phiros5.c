42fde7a75c201b57c4086db08d008364043bd704
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 23:01:59 +0100

    Update phiros5.c

--- phiros5.c
+++ phiros5.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "phiros.h"
 
 /**
- * add_sep_node_end - add up a separator found
+ * pr_add_sep_node_end - add up a separator found
  * at the end of a sep_list.
  * @head: head of inked list.
  * @sep: separator found (; | &).
  * Return: address of the head.
  */
-sep_list *add_sep_node_end(sep_list **head, char sep)
+sep_list *pr_add_sep_node_end(sep_list **head, char sep)
 {
 	sep_list *new, *temp;
 
@@ -34,11 +34,11 @@ sep_list *add_sep_node_end(sep_list **head, char sep)
 }
 
 /**
- * free_sep_list - frees up the a sep_list
+ * pr_free_sep_list - frees up the a sep_list
  * @head: head of linked list.
  * Return: no return.
  */
-void free_sep_list(sep_list **head)
+void pr_free_sep_list(sep_list **head)
 {
 	sep_list *temp;
 	sep_list *curr;
@@ -56,13 +56,13 @@ void free_sep_list(sep_list **head)
 }
 
 /**
- * add_line_node_end - add up a command line
+ * pr_add_line_node_end - add up a command line
  * at the end of a line_list.
  * @head: head of linked list.
  * @line: a command line.
  * Return: the address of the head.
  */
-line_list *add_line_node_end(line_list **head, char *line)
+line_list *pr_add_line_node_end(line_list **head, char *line)
 {
 	line_list *new, *temp;
 
@@ -89,11 +89,11 @@ line_list *add_line_node_end(line_list **head, char *line)
 }
 
 /**
- * free_line_list - frees up a line_list
+ * pr_free_line_list - frees up a line_list
  * @head: head of linked list.
  * Return: no return.
  */
-void free_line_list(line_list **head)
+void pr_free_line_list(line_list **head)
 {
 	line_list *temp;
 	line_list *curr;
