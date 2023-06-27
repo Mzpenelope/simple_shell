d83eac0f7e19dd0b3b75c2c391853adca8676046
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 17:43:05 +0100

    Update phiros19.c

--- phiros19.c
+++ phiros19.c
@@ -1,31 +1,30 @@
-#include "main.h"
+#include "phiros.h"
 
 /**
- * exit_shell - exits the shell
- *
- * @datash: data relevant (status and args)
+ * pr_exit_shell - exits the shell
+ * @dsh: data relevant (status and args)
  * Return: 0 on success.
  */
-int exit_shell(data_shell *datash)
+int pr_exit_shell(phiros_shell *dsh)
 {
-	unsigned int ustatus;
-	int is_digit;
-	int str_len;
-	int big_number;
+	unsigned int us;
+	int is_dig;
+	int strle_n;
+	int big_num;
 
-	if (datash->args[1] != NULL)
+	if (dsh->args[1] != NULL)
 	{
-		ustatus = _atoi(datash->args[1]);
-		is_digit = _isdigit(datash->args[1]);
-		str_len = _strlen(datash->args[1]);
-		big_number = ustatus > (unsigned int)INT_MAX;
-		if (!is_digit || str_len > 10 || big_number)
+		us = pr_atoi(dsh->args[1]);
+		is_dig = pr_isdigit(dsh->args[1]);
+		strle_n = pr_strlen(dsh->args[1]);
+		big_num = us > (unsigned int)INT_MAX;
+		if (!is_dig || strle_n > 10 || big_num)
 		{
-			get_error(datash, 2);
-			datash->status = 2;
+			pr_get_error(dsh, 2);
+			dsh->status = 2;
 			return (1);
 		}
-		datash->status = (ustatus % 256);
+		dsh->status = (us % 256);
 	}
 	return (0);
 }
