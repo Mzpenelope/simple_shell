f4b45589006e3e6fab78c953b8b51167cc522595
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 15:30:38 +0100

    Update phiros1.c

--- phiros1.c
+++ phiros1.c
@@ -1,144 +1,138 @@
-#include "main.h"
-
+#include "phiros.h"
 /**
- * strcat_cd - function that concatenates the message for cd error
- *
- * @datash: data relevant (directory)
+ * pr_strcat_cd - concatenates the msg for cd error
+ * @dsh: data relevant (directory)
  * @msg: message to print
- * @error: output message
- * @ver_str: counter lines
+ * @e: output message
+ * @v_s: counter lines
  * Return: error message
- */
-char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
+ ****************************************************************/
+char *pr_strcat_cd(phiros_shell *dsh, char *msg, char *e, char *v_s)
 {
-	char *illegal_flag;
+char *bad_flag;
 
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, msg);
-	if (datash->args[1][0] == '-')
-	{
-		illegal_flag = malloc(3);
-		illegal_flag[0] = '-';
-		illegal_flag[1] = datash->args[1][1];
-		illegal_flag[2] = '\0';
-		_strcat(error, illegal_flag);
-		free(illegal_flag);
-	}
-	else
-	{
-		_strcat(error, datash->args[1]);
-	}
-
-	_strcat(error, "\n");
-	_strcat(error, "\0");
-	return (error);
+pr_strcpy(e, dsh->av[0]);
+pr_strcat(e, ": ");
+pr_strcat(e, v_s);
+pr_strcat(e, ": ");
+pr_strcat(e, dsh->args[0]);
+pr_strcat(e, msg);
+if (dsh->args[1][0] == '-')
+{
+bad_flag = malloc(3);
+bad_flag[0] = '-';
+bad_flag[1] = dsh->args[1][1];
+bad_flag[2] = '\0';
+pr_strcat(error, bad_flag);
+free(bad_flag);
+}
+else
+{
+pr_strcat(e, dsh->args[1]);
 }
 
+pr_strcat(e, "\n");
+pr_strcat(e, "\0");
+return (e);
+}
 /**
- * error_get_cd - error message for cd command in get_cd
- * @datash: data relevant (directory)
+ * e_g_cd - error message for cd command in get_cd
+ * @dsh: data relevant (directory)
  * Return: Error message
- */
-char *error_get_cd(data_shell *datash)
+ *****************************************************************/
+char *e_g_cd(phiros_shell *dsh)
 {
-	int length, len_id;
-	char *error, *ver_str, *msg;
+int lth, lth_id;
+char *e, *v_s, *msg;
 
-	ver_str = aux_itoa(datash->counter);
-	if (datash->args[1][0] == '-')
-	{
-		msg = ": Illegal option ";
-		len_id = 2;
-	}
-	else
-	{
-		msg = ": can't cd to ";
-		len_id = _strlen(datash->args[1]);
-	}
-
-	length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
-	length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
-	error = malloc(sizeof(char) * (length + 1));
-
-	if (error == 0)
-	{
-		free(ver_str);
-		return (NULL);
-	}
-
-	error = strcat_cd(datash, msg, error, ver_str);
-
-	free(ver_str);
-
-	return (error);
+v_s = pr_itoa(dsh->counter);
+if (dsh->args[1][0] == '-')
+{
+msg = ": Bad option ";
+lth_id = 2;
+}
+else
+{
+msg = ": cannot cd to ";
+lth_id = pr_strlen(dsh->args[1]);
 }
 
-/**
- * error_not_found - generic error message for command not found
- * @datash: data relevant (counter, arguments)
- * Return: Error message
- */
-char *error_not_found(data_shell *datash)
-{
-	int length;
-	char *error;
-	char *ver_str;
+lth = pr_strlen(dsh->av[0]) + pr_strlen(dsh->args[0]);
+lth += pr_strlen(v_s) + pr_strlen(msg) + lth_id + 5;
+e = malloc(sizeof(char) * (lth + 1));
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + 16;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
-	{
-		free(error);
-		free(ver_str);
-		return (NULL);
-	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": not found\n");
-	_strcat(error, "\0");
-	free(ver_str);
-	return (error);
+if (e == 0)
+{
+free(v_s);
+return (NULL);
 }
 
+e = pr_strcat_cd(dsh, msg, e, v_s);
+
+free(v_s);
+
+return (e);
+}
 /**
- * error_exit_shell - generic error message for exit in get_exit
- * @datash: data relevant (counter, arguments)
- *
+ * e_n_f - generic error message for command not found
+ * @dsh: data relevant (counter, arguments)
  * Return: Error message
  */
-char *error_exit_shell(data_shell *datash)
+char *e_n_f(phiros_shell *dsh)
 {
-	int length;
-	char *error;
-	char *ver_str;
+int lth;
+char *e;
+char *v_s;
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
-	{
-		free(ver_str);
-		return (NULL);
-	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": Illegal number: ");
-	_strcat(error, datash->args[1]);
-	_strcat(error, "\n\0");
-	free(ver_str);
+v_s = pr_itoa(dsh->counter);
+lth = pr_strlen(dsh->av[0]) + pr_strlen(v_s);
+lth += pr_strlen(dsh->args[0]) + 16;
+e = malloc(sizeof(char) * (lth + 1));
+if (e == 0)
+{
+free(e);
+free(v_s);
+return (NULL);
+}
+pr_strcpy(e, dsh->av[0]);
+pr_strcat(e, ": ");
+pr_strcat(e, v_s);
+pr_strcat(e, ": ");
+pr_strcat(e, dsh->args[0]);
+pr_strcat(e, ": not found\n");
+pr_strcat(e, "\0");
+free(v_s);
+return (e);
+}
+/**
+ * e_e_s - generic error message for exit in get_exit
+ * @dsh: data relevant (counter, arguments)
+ * Return: Error message
+ ***********************************************************/
+char *e_e_s(phiros_shell *dsh)
+{
+int lth;
+char *e;
+char *v_s;
 
-	return (error);
+v_s = pr_itoa(dsh->counter);
+lth = pr_strlen(dsh->av[0]) + pr_strlen(v_s);
+lth += pr_strlen(dsh->args[0]) + pr_strlen(dsh->args[1]) + 23;
+e = malloc(sizeof(char) * (lth + 1));
+if (e == 0)
+{
+free(v_s);
+return (NULL);
+}
+pr_strcpy(e, dsh->av[0]);
+pr_strcat(e, ": ");
+pr_strcat(e, v_s);
+pr_strcat(e, ": ");
+pr_strcat(e, dsh->args[0]);
+pr_strcat(e, ": Bad number: ");
+pr_strcat(e, dsh->args[1]);
+pr_strcat(e, "\n\0");
+free(v_s);
+
+return (e);
 }
