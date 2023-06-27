5b35f7579cffd975c56b14830482831659175b6f
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 16:58:28 +0100

    Update phiros12.c

--- phiros12.c
+++ phiros12.c
@@ -1,165 +1,157 @@
-#include "main.h"
-
+#include "phiros.h"
 /**
- * cd_dot - changes to the parent directory
- *
- * @datash: data relevant (environ)
- *
+ * pr_cd_dot - changes to the parent directory
+ * @dsh: data relevant (environ)
  * Return: no return
  */
-void cd_dot(data_shell *datash)
+void pr_cd_dot(phiros_shell *dsh)
 {
 	char pwd[PATH_MAX];
-	char *dir, *cp_pwd, *cp_strtok_pwd;
+	char *d, *cp_pwd, *cp_strtok_pwd;
 
 	getcwd(pwd, sizeof(pwd));
-	cp_pwd = _strdup(pwd);
-	set_env("OLDPWD", cp_pwd, datash);
-	dir = datash->args[1];
-	if (_strcmp(".", dir) == 0)
+	cp_pwd = pr_strdup(pwd);
+	pr_set_env("OLDPWD", cp_pwd, dsh);
+	d = dsh->args[1];
+	if (pr_strcmp(".", d) == 0)
 	{
-		set_env("PWD", cp_pwd, datash);
+		pr_set_env("PWD", cp_pwd, dsh);
 		free(cp_pwd);
 		return;
 	}
-	if (_strcmp("/", cp_pwd) == 0)
+	if (pr_strcmp("/", cp_pwd) == 0)
 	{
 		free(cp_pwd);
 		return;
 	}
 	cp_strtok_pwd = cp_pwd;
-	rev_string(cp_strtok_pwd);
-	cp_strtok_pwd = _strtok(cp_strtok_pwd, "/");
+	pr_rev_string(cp_strtok_pwd);
+	cp_strtok_pwd = pr_strtok(cp_strtok_pwd, "/");
 	if (cp_strtok_pwd != NULL)
 	{
-		cp_strtok_pwd = _strtok(NULL, "\0");
+		cp_strtok_pwd = pr_strtok(NULL, "\0");
 
 		if (cp_strtok_pwd != NULL)
-			rev_string(cp_strtok_pwd);
+			pr_rev_string(cp_strtok_pwd);
 	}
 	if (cp_strtok_pwd != NULL)
 	{
 		chdir(cp_strtok_pwd);
-		set_env("PWD", cp_strtok_pwd, datash);
+		pr_set_env("PWD", cp_strtok_pwd, dsh);
 	}
 	else
 	{
 		chdir("/");
-		set_env("PWD", "/", datash);
+		pr_set_env("PWD", "/", dsh);
 	}
-	datash->status = 0;
+	dsh->status = 0;
 	free(cp_pwd);
 }
 
 /**
- * cd_to - changes to a directory given
- * by the user
- *
- * @datash: data relevant (directories)
+ * pr_cd_to - changes to a dir given by user
+ * @dsh: data relevant (directories)
  * Return: no return
  */
-void cd_to(data_shell *datash)
+void pr_cd_to(phiros_shell *dsh)
 {
 	char pwd[PATH_MAX];
-	char *dir, *cp_pwd, *cp_dir;
+	char *d, *cp_pwd, *cp_dir;
 
 	getcwd(pwd, sizeof(pwd));
 
-	dir = datash->args[1];
-	if (chdir(dir) == -1)
+	d = dsh->args[1];
+	if (chdir(d) == -1)
 	{
-		get_error(datash, 2);
+		pr_get_error(dsh, 2);
 		return;
 	}
 
-	cp_pwd = _strdup(pwd);
-	set_env("OLDPWD", cp_pwd, datash);
+	cp_pwd = pr_strdup(pwd);
+	pr_set_env("OLDPWD", cp_pwd, dsh);
 
-	cp_dir = _strdup(dir);
-	set_env("PWD", cp_dir, datash);
+	cp_dir = pr_strdup(d);
+	pr_set_env("PWD", cp_dir, dsh);
 
 	free(cp_pwd);
 	free(cp_dir);
 
-	datash->status = 0;
+	dsh->status = 0;
 
-	chdir(dir);
+	chdir(d);
 }
-
 /**
- * cd_previous - changes to the previous directory
- *
- * @datash: data relevant (environ)
+ * pr_cd_previous - changes to the previous directory
+ * @dsh: data relevant (environ)
  * Return: no return
  */
-void cd_previous(data_shell *datash)
+void pr_cd_previous(phiros_shell *dsh)
 {
 	char pwd[PATH_MAX];
 	char *p_pwd, *p_oldpwd, *cp_pwd, *cp_oldpwd;
 
 	getcwd(pwd, sizeof(pwd));
-	cp_pwd = _strdup(pwd);
+	cp_pwd = pr_strdup(pwd);
 
-	p_oldpwd = _getenv("OLDPWD", datash->_environ);
+	p_oldpwd = pr_getenv("OLDPWD", dsh->_environ);
 
 	if (p_oldpwd == NULL)
 		cp_oldpwd = cp_pwd;
 	else
-		cp_oldpwd = _strdup(p_oldpwd);
+		cp_oldpwd = pr_strdup(p_oldpwd);
 
-	set_env("OLDPWD", cp_pwd, datash);
+	pr_set_env("OLDPWD", cp_pwd, dsh);
 
 	if (chdir(cp_oldpwd) == -1)
-		set_env("PWD", cp_pwd, datash);
+		pr_set_env("PWD", cp_pwd, dsh);
 	else
-		set_env("PWD", cp_oldpwd, datash);
+		pr_set_env("PWD", cp_oldpwd, dsh);
 
-	p_pwd = _getenv("PWD", datash->_environ);
+	p_pwd = pr_getenv("PWD", dsh->_environ);
 
-	write(STDOUT_FILENO, p_pwd, _strlen(p_pwd));
+	write(STDOUT_FILENO, p_pwd, pr_strlen(p_pwd));
 	write(STDOUT_FILENO, "\n", 1);
 
 	free(cp_pwd);
 	if (p_oldpwd)
 		free(cp_oldpwd);
 
-	datash->status = 0;
+	dsh->status = 0;
 
 	chdir(p_pwd);
 }
 
 /**
- * cd_to_home - changes to home directory
- *
+ * pr_cd_to_home - changes to home directory
  * @datash: data relevant (environ)
  * Return: no return
  */
-void cd_to_home(data_shell *datash)
+void pr_cd_to_home(phiros_shell *dsh)
 {
 	char *p_pwd, *home;
 	char pwd[PATH_MAX];
 
 	getcwd(pwd, sizeof(pwd));
-	p_pwd = _strdup(pwd);
+	p_pwd = pr_strdup(pwd);
 
-	home = _getenv("HOME", datash->_environ);
+	home = pr_getenv("HOME", sh->_environ);
 
 	if (home == NULL)
 	{
-		set_env("OLDPWD", p_pwd, datash);
+		pr_set_env("OLDPWD", p_pwd, dsh);
 		free(p_pwd);
 		return;
 	}
 
 	if (chdir(home) == -1)
 	{
-		get_error(datash, 2);
+		pr_get_error(dsh, 2);
 		free(p_pwd);
 		return;
 	}
 
-	set_env("OLDPWD", p_pwd, datash);
-	set_env("PWD", home, datash);
+	pr_set_env("OLDPWD", p_pwd, dsh);
+	pr_set_env("PWD", home, dsh);
 	free(p_pwd);
-	datash->status = 0;
+	dsh->status = 0;
 }
