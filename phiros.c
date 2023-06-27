10f88b1187a0f0c1d8dfb21d2ce582555297bd08
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 14:44:34 +0100

    Update phiros.c

--- phiros.c
+++ phiros.c
@@ -1,73 +1,70 @@
 #include "main.h"
-
 /**
- * free_data - frees data structure
- *
- * @datash: data structure
+ * f_data - frees data struct
+ * @dsh: data struct
  * Return: no return
- */
-void free_data(data_shell *datash)
+ **********************************/
+void f_data(phiros_shell *dsh)
 {
-	unsigned int i;
-
-	for (i = 0; datash->_environ[i]; i++)
-	{
-		free(datash->_environ[i]);
-	}
-
-	free(datash->_environ);
-	free(datash->pid);
+unsigned int u = 0;
+while (dsh->_environ[u])
+{
+free(dsh->_environ[u]);
+u++;
 }
 
+free(dsh->_environ);
+free(dsh->pid);
+}
 /**
- * set_data - Initialize data structure
- *
- * @datash: data structure
- * @av: argument vector
+ * s_data - Initialize data struct
+ * @dsh: data struct
+ * @av: arg vector
  * Return: no return
- */
-void set_data(data_shell *datash, char **av)
+ **********************************/
+void s_data(phiros_shell *dsh, char **av)
 {
-	unsigned int i;
+unsigned int u = 0;
 
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
+dsh->av = av;
+dsh->input = NULL;
+dsh->args = NULL;
+dsh->status = 0;
+dsh->counter = 1;
 
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
+while (environ[u])
+{
+u++;
 }
 
+dsh->_environ = malloc(sizeof(char *) * (u + 1));
+
+int i = 0;
+while (environ[i])
+{
+dsh->_environ[i] = pr_strdup(environ[i]);
+i++;
+}
+
+dsh->_environ[i] = NULL;
+dsh->pid = pr_itoa(getpid());
+}
 /**
  * main - Entry point
- *
  * @ac: argument count
  * @av: argument vector
- *
  * Return: 0 on success.
  */
 int main(int ac, char **av)
 {
-	data_shell datash;
+	phiros_shell dsh;
 	(void) ac;
 
 	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
+	s_data(&dsh, av);
+	s_loop(&dsh);
+	f_data(&dsh);
+	if (dsh.status < 0)
 		return (255);
-	return (datash.status);
+	return (dsh.status);
 }
