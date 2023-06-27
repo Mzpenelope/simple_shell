2c76aca3e907779c71b25e1c5f0807e74925e195
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 17:29:00 +0100

    Update phiros14.c

--- phiros14.c
+++ phiros14.c
@@ -1,175 +1,168 @@
-#include "main.h"
+#include "phiros.h"
 
 /**
- * repeated_char - counts the repetitions of a char
- *
+ * repeat_char - counts the repetitions of a char
  * @input: input string
- * @i: index
+ * @u: index
  * Return: repetitions
  */
-int repeated_char(char *input, int i)
+int repeat_char(char *input, int u)
 {
 	if (*(input - 1) == *input)
-		return (repeated_char(input - 1, i + 1));
+		return (repeated_char(input - 1, u + 1));
 
-	return (i);
+	return (u);
 }
-
 /**
- * error_sep_op - finds syntax errors
- *
+ * pr_e_s_p - finds syntax errors
  * @input: input string
- * @i: index
+ * @u: index
  * @last: last char read
  * Return: index of error. 0 when there are no
  * errors
  */
-int error_sep_op(char *input, int i, char last)
+int pr_e_s_p(char *input, int u, char last)
 {
-	int count;
+	int c;
 
-	count = 0;
+	c = 0;
 	if (*input == '\0')
 		return (0);
 
 	if (*input == ' ' || *input == '\t')
-		return (error_sep_op(input + 1, i + 1, last));
+		return (pr_e_s_p(input + 1, u + 1, last));
 
 	if (*input == ';')
 		if (last == '|' || last == '&' || last == ';')
-			return (i);
+			return (u);
 
 	if (*input == '|')
 	{
 		if (last == ';' || last == '&')
-			return (i);
+			return (u);
 
 		if (last == '|')
 		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-				return (i);
+			c = repeat_char(input, 0);
+			if (c == 0 || c > 1)
+				return (u);
 		}
 	}
 
 	if (*input == '&')
 	{
 		if (last == ';' || last == '|')
-			return (i);
+			return (u);
 
 		if (last == '&')
 		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-				return (i);
+			c = repeat_char(input, 0);
+			if (c == 0 || c > 1)
+				return (u);
 		}
 	}
 
-	return (error_sep_op(input + 1, i + 1, *input));
+	return (pr_e_s_p(input + 1, u + 1, *input));
 }
-
 /**
- * first_char - finds index of the first char
- *
+ * pr_first_char - finds index of the first char
  * @input: input string
- * @i: index
+ * @u: index
  * Return: 1 if there is an error. 0 in other case.
  */
-int first_char(char *input, int *i)
+int pr_first_char(char *input, int *u)
+int u = 0;
+while (input[u])
 {
-
-	for (*i = 0; input[*i]; *i += 1)
-	{
-		if (input[*i] == ' ' || input[*i] == '\t')
-			continue;
-
-		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
-			return (-1);
-
-		break;
-	}
-
-	return (0);
+if (input[u] == ' ' || input[u] == '\t')
+{
+continue;
 }
 
+if (input[u] == ';' || input[u] == '|' || input[u] == '&') {
+return (-1);
+}
+
+break;
+u++;
+}
+
+return (0);
 /**
- * print_syntax_error - prints when a syntax error is found
- *
- * @datash: data structure
+ * p_s_e - prints when a syntax error is found
+ * @dsh: data structure
  * @input: input string
- * @i: index of the error
+ * @u: index of the error
  * @bool: to control msg error
  * Return: no return
  */
-void print_syntax_error(data_shell *datash, char *input, int i, int bool)
+void p_s_e(phiros_shell *dsh, char *input, int u, int bool)
 {
-	char *msg, *msg2, *msg3, *error, *counter;
-	int length;
+	char *msg, *msg2, *msg3, *e, *counter;
+	int lth;
 
-	if (input[i] == ';')
+	if (input[u] == ';')
 	{
 		if (bool == 0)
-			msg = (input[i + 1] == ';' ? ";;" : ";");
+			msg = (input[u + 1] == ';' ? ";;" : ";");
 		else
-			msg = (input[i - 1] == ';' ? ";;" : ";");
+			msg = (input[u - 1] == ';' ? ";;" : ";");
 	}
 
-	if (input[i] == '|')
-		msg = (input[i + 1] == '|' ? "||" : "|");
+	if (input[u] == '|')
+		msg = (input[u + 1] == '|' ? "||" : "|");
 
-	if (input[i] == '&')
-		msg = (input[i + 1] == '&' ? "&&" : "&");
+	if (input[u] == '&')
+		msg = (input[u + 1] == '&' ? "&&" : "&");
 
 	msg2 = ": Syntax error: \"";
 	msg3 = "\" unexpected\n";
-	counter = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(counter);
-	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
+	counter = pr_itoa(dsh->counter);
+	lth = pr_strlen(dsh->av[0]) + pr_strlen(counter);
+	lth += pr_strlen(msg) + pr_strlen(msg2) + pr_strlen(msg3) + 2;
 
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	e = malloc(sizeof(char) * (lth + 1));
+	if (e == 0)
 	{
 		free(counter);
 		return;
 	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, counter);
-	_strcat(error, msg2);
-	_strcat(error, msg);
-	_strcat(error, msg3);
-	_strcat(error, "\0");
+	pr_strcpy(e, dsh->av[0]);
+	pr_strcat(e, ": ");
+	pr_strcat(e, counter);
+	pr_strcat(e, msg2);
+	pr_strcat(e, msg);
+	pr_strcat(e, msg3);
+	pr_strcat(e, "\0");
 
-	write(STDERR_FILENO, error, length);
-	free(error);
+	write(STDERR_FILENO, e, lth);
+	free(e);
 	free(counter);
 }
-
 /**
- * check_syntax_error - intermediate function to
+ * c_s_e - intermediate function to
  * find and print a syntax error
- *
- * @datash: data structure
+ * @dsh: data structure
  * @input: input string
  * Return: 1 if there is an error. 0 in other case
  */
-int check_syntax_error(data_shell *datash, char *input)
+int c_s_e(phiros_shell *dsh, char *input)
 {
-	int begin = 0;
+	int b = 0;
 	int f_char = 0;
 	int i = 0;
 
-	f_char = first_char(input, &begin);
+	f_char = pr_first_char(input, &b);
 	if (f_char == -1)
 	{
-		print_syntax_error(datash, input, begin, 0);
+		p_s_e(dsh, input, b, 0);
 		return (1);
 	}
 
-	i = error_sep_op(input + begin, 0, *(input + begin));
+	i = pr_e_s_p(input + b, 0, *(input + b));
 	if (i != 0)
 	{
-		print_syntax_error(datash, input, begin + i, 1);
+		p_s_e(dsh, input, b + i, 1);
 		return (1);
 	}
 
