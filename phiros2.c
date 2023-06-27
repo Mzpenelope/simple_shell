f9a2be44b722c5ca338f995cd4be89f83f1f9a07
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-28 00:43:55 +0100

    Update phiros2.c

--- phiros2.c
+++ phiros2.c
@@ -1,69 +1,67 @@
-#include "main.h"
-
+#include "phiros.h"
 /**
  * error_env - the error info for env in get_env.
  * @dsh: data relevant (counter, arguments)
  * Return: an error message.
  */
-char *error_env(data_shell *dsh)
+char *e_e(phiros_shell *dsh)
 {
-	int length;
-	char *error;
-	char *ver_str;
+	int lth;
+	char *e;
+	char *v_s;
 	char *msg;
 
-	ver_str = aux_itoa(dsh->counter);
+	v_s = pr_itoa(dsh->counter);
 	msg = ": Unable to add/remove from environment\n";
-	length = pr_strlen(dsh->av[0]) + pr_strlen(ver_str);
-	length += pr_strlen(dsh->args[0]) + pr_strlen(msg) + 4;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	lth = pr_strlen(dsh->av[0]) + pr_strlen(v_s);
+	lth += pr_strlen(dsh->args[0]) + pr_strlen(msg) + 4;
+	e = malloc(sizeof(char) * (lth + 1));
+	if (e == 0)
 	{
-		free(error);
-		free(ver_str);
+		free(e);
+		free(v_s);
 		return (NULL);
 	}
 
-	pr_strcpy(error, dsh->av[0]);
-	pr_strcat(error, ": ");
-	pr_strcat(error, ver_str);
-	pr_strcat(error, ": ");
-	pr_strcat(error, ds->args[0]);
-	pr_strcat(error, msg);
-	pr_strcat(error, "\0");
-	free(ver_str);
+	pr_strcpy(e, dsh->av[0]);
+	pr_strcat(e, ": ");
+	pr_strcat(e, v_s);
+	pr_strcat(e, ": ");
+	pr_strcat(e, ds->args[0]);
+	pr_strcat(e, msg);
+	pr_strcat(e, "\0");
+	free(v_s);
 
-	return (error);
+	return (e);
 }
 /**
- * error_path_126 - error info for path and failure denied permission.
+ * e_p_126 - error info for path and failure denied permission.
  * @dsh: data relevant (counter, arguments).
- *
  * Return: error string.
  */
-char *error_path_126(data_shell *dsh)
+char *e_p_126(phiros_shell *dsh)
 {
-	int length;
-	char *ver_str;
-	char *error;
+	int lth;
+	char *v_s;
+	char *e;
 
-	ver_str = aux_itoa(dsh->counter);
-	length = pr_strlen(dsh->av[0]) + pr_strlen(ver_str);
-	length += pr_strlen(dsh->args[0]) + 24;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	v_s = pr_itoa(dsh->counter);
+	lth = pr_strlen(dsh->av[0]) + pr_strlen(v_s);
+	lth += pr_strlen(dsh->args[0]) + 24;
+	e = malloc(sizeof(char) * (lth + 1));
+	if (e == 0)
 	{
-		free(error);
-		free(ver_str);
+		free(e);
+		free(v_s);
 		return (NULL);
 	}
-	pr_strcpy(error, dsh->av[0]);
-	pr_strcat(error, ": ");
-	pr_strcat(error, ver_str);
-	pr_strcat(error, ": ");
-	pr_strcat(error, dsh->args[0]);
-	pr_strcat(error, ": Permission denied\n");
-	pr_strcat(error, "\0");
-	free(ver_str);
-	return (error);
+	pr_strcpy(e, dsh->av[0]);
+	pr_strcat(e, ": ");
+	pr_strcat(e, v_s);
+	pr_strcat(e, ": ");
+	pr_strcat(e, dsh->args[0]);
+	pr_strcat(e, ": Permission denied\n");
+	pr_strcat(e, "\0");
+	free(v_s);
+	return (e);
 }
