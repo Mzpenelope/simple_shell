f666b14c55e1b390ab4f9f5c84c839796df78598
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-27 23:58:01 +0100

    Update phiros16.c

--- phiros16.c
+++ phiros16.c
@@ -1,20 +1,18 @@
-#include "main.h"
-
+#include "phiros.h"
 /**
- * cmp_env_name - compares env variables names
+ * pr_cmp_env_name - compares env variables names
  * with the name passed.
  * @nenv: name of the environment variable
  * @name: name passed
- *
  * Return: 0 if are not equal. Another value if they are.
  */
-int cmp_env_name(const char *nenv, const char *name)
+int pr_cmp_env_name(const char *nenv, const char *name)
 {
-	int i;
+	int m;
 
-	for (i = 0; nenv[i] != '='; i++)
+	for (m = 0; nenv[m] != '='; m++)
 	{
-		if (nenv[i] != name[i])
+		if (nenv[m] != name[m])
 		{
 			return (0);
 		}
@@ -22,59 +20,52 @@ int cmp_env_name(const char *nenv, const char *name)
 
 	return (i + 1);
 }
-
 /**
- * _getenv - get an environment variable
+ * pr_getenv - get an environment variable
  * @name: name of the environment variable
  * @_environ: environment variable
- *
  * Return: value of the environment variable if is found.
  * In other case, returns NULL.
  */
-char *_getenv(const char *name, char **_environ)
+char *pr_getenv(const char *name, char **_environ)
 {
 	char *ptr_env;
-	int i, mov;
+	int n, mov;
 
-	/* Initialize ptr_env value */
 	ptr_env = NULL;
 	mov = 0;
-	/* Compare all environment variables */
-	/* environ is declared in the header file */
-	for (i = 0; _environ[i]; i++)
+	
+	for (n = 0; _environ[n]; n++)
 	{
-		/* If name and env are equal */
-		mov = cmp_env_name(_environ[i], name);
+		mov = pr_cmp_env_name(_environ[n], name);
 		if (mov)
 		{
-			ptr_env = _environ[i];
+			ptr_env = _environ[n];
 			break;
 		}
 	}
 
 	return (ptr_env + mov);
 }
-
 /**
- * _env - prints the evironment variables
- *
- * @datash: data relevant.
+ * pr_env - prints the evironment variables
+ * @dsh: data relevant.
  * Return: 1 on success.
  */
-int _env(data_shell *datash)
+int pr_env(phiros_shell *dsh)
 {
-	int i, j;
+	int g, h;
 
-	for (i = 0; datash->_environ[i]; i++)
+	for (g = 0; dsh->_environ[g]; g++)
 	{
 
-		for (j = 0; datash->_environ[i][j]; j++)
+		for (h = 0; dsh->_environ[g][h]; h++)
 			;
 
-		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, dsh->_environ[g], h);
 		write(STDOUT_FILENO, "\n", 1);
 	}
-	datash->status = 0;
+	dsh->status = 0;
 
 	return (1);
 }
