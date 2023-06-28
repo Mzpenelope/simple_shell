329c7223f16b3d6cb4379350862faadde0f1ef54
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-28 01:09:25 +0100

    Update phiros11.c

--- phiros11.c
+++ phiros11.c
@@ -6,24 +6,26 @@
  * Return: no return.
  */
 void pr_rev_string(char *s)
-int count = 0, m, n;
-char *string, temp;
+{
+  int count = 0, m, n;
+  char *string, temp;
 
-for (count = 0; s[count] != '\0'; count++)
-{
-string = s;
-}
+  for (count = 0; s[count] != '\0'; count++)
+  {
+  string = s;
+  }
 
-m = 0;
-while (m < count - 1)
-{
-n = m + 1;
-while (n > 0)
-{
-temp = *(string + n);
-*(string + n) = *(string + (n - 1));
-*(string + (n - 1)) = temp;
-n--;
-}
-m++;
+  m = 0;
+  while (m < count - 1)
+  {
+  n = m + 1;
+  while (n > 0)
+  {
+        temp = *(string + n);
+        *(string + n) = *(string + (n - 1));
+        *(string + (n - 1)) = temp;
+        n--;
+  }
+    m++;
+  }
 }
