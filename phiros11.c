9329ab62e9e6f8356d8ed938e46884b1818ad688
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-06-28 01:36:33 +0100

    Update phiros11.c

--- phiros11.c
+++ phiros11.c
@@ -1,5 +1,4 @@
 #include "phiros.h"
-
 /**
  * pr_rev_string - reverses a string.
  * @s: input string.
@@ -7,25 +6,25 @@
  */
 void pr_rev_string(char *s)
 {
-  int count = 0, m, n;
-  char *string, temp;
+int count = 0, m, n;
+char *string, temp;
 
-  for (count = 0; s[count] != '\0'; count++)
-  {
-  string = s;
-  }
+for (count = 0; s[count] != '\0'; count++)
+{
+string = s;
+}
 
-  m = 0;
-  while (m < count - 1)
-  {
-  n = m + 1;
-  while (n > 0)
-  {
-        temp = *(string + n);
-        *(string + n) = *(string + (n - 1));
-        *(string + (n - 1)) = temp;
-        n--;
-  }
-    m++;
-  }
+m = 0;
+while (m < count - 1)
+{
+n = m + 1;
+while (n > 0)
+{
+temp = *(string + n);
+*(string + n) = *(string + (n - 1));
+*(string + (n - 1)) = temp;
+n--;
+}
+m++;
+}
 }
